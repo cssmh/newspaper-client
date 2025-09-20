da233948dd0e1e106ce16bf7369ca5b485e749c8
Momen <mominitmbstu@gmail.com>
2025-09-20 13:02:23 +0600

    old merged code

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import Root from "./Root/Root.jsx";
 import { RouterProvider } from "react-router-dom";
 import AuthProviders from "./AuthProviders/AuthProviders.jsx";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
